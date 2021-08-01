33246040d6ad0d3035c66633968620dc583eb479
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2021-08-01 20:36:06 +0100

    model issue fix on heroku

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    currentAbsolutePath = os.path.dirname(os.path.abspath(__file__))
+
+    modelPath = currentAbsolutePath + '/machine_learning_files/model.pkl'
+
+    model = joblib.load(modelPath)  # Load model.pkl
+    print('Model loaded')
+
+    modelColumnsPath = currentAbsolutePath + '/machine_learning_files/model_columns.pkl'
+
+    model_columns = joblib.load(modelColumnsPath)  # Load model_columns.pkl
+    print('Model columns loaded')
+
     if model:
         try:
             json_ = request.json
@@ -34,22 +46,25 @@ def predict():
 
 
 if __name__ == '__main__':
-    try:
-        port = int(sys.argv[1])  # This is for a command line input
-    except:
-        port = 3000  # if you don't provide any port the port will be set to 1
+    # try:
+    #     port = int(sys.argv[1])  # This is for a command line input
+    # except:
+    #     port = 3000  # if you don't provide any port the port will be set to 1
 
-    currentAbsolutePath = os.path.dirname(os.path.abspath(__file__))
 
-    modelPath = currentAbsolutePath + '/machine_learning_files/model.pkl'
-
-    model = joblib.load(modelPath)  # Load model.pkl
-    print('Model loaded')
-
-    modelColumnsPath = currentAbsolutePath + '/machine_learning_files/model_columns.pkl'
-
-    model_columns = joblib.load(modelColumnsPath)  # Load model_columns.pkl
-    print('Model columns loaded')
+    # @app.before_first_request
+    # def load_machine_learning_models():
+    #     currentAbsolutePath = os.path.dirname(os.path.abspath(__file__))
+    #
+    #     modelPath = currentAbsolutePath + '/machine_learning_files/model.pkl'
+    #
+    #     model = joblib.load(modelPath)  # Load model.pkl
+    #     print('Model loaded')
+    #
+    #     modelColumnsPath = currentAbsolutePath + '/machine_learning_files/model_columns.pkl'
+    #
+    #     model_columns = joblib.load(modelColumnsPath)  # Load model_columns.pkl
+    #     print('Model columns loaded')
 
     print('API Starting...')
-    app.run(port=port, debug=True)
+    app.run(port=3000, debug=True)
